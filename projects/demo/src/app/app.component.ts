1776e08da8c12635f10d0b37ac315e3c3d5a56f9
iton0 <iton442@gmail.com>
2024-09-07 17:37:51 -0400

    fix: library package not building
    - removed deprecated umdModuleIds config from ng-business-hours's
    ng-package.json
      - For more details, see: https://stackoverflow.com/questions/70309982/configuration-doesnt-match-the-required-schema-data-path-lib-must-not-have"
    - added angular cli cache folder to gitignore
    
    Should be able to build and serve application on locally cloned repo.

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,26 +1,24 @@
-import {Component} from '@angular/core';
-import {version} from '../../../../package.json';
-import {LocaleService} from './locale.service';
-import {NgBusinessHoursLocaleService} from '../../../ng-business-hours/src/lib/ng-business-hours-locale.service';
+import { Component } from '@angular/core';
+import { LocaleService } from './locale.service';
+import { NgBusinessHoursLocaleService } from '../../../ng-business-hours/src/lib/ng-business-hours-locale.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'demo';
-  version = version;
   businessHours = [];
   disabled = false;
   isoWeek = true;
-  validationErrorMessageRu = '"Начало" рабочего дня должно быть раньше чем "Конец"';
+  validationErrorMessageRu =
+    '"Начало" рабочего дня должно быть раньше чем "Конец"';
 
   constructor(
     private localeService: LocaleService,
-    private ngBusinessHoursLocaleService: NgBusinessHoursLocaleService
-  ) {
-  }
+    private ngBusinessHoursLocaleService: NgBusinessHoursLocaleService,
+  ) {}
 
   getLocale(): string {
     return this.localeService.locale;
